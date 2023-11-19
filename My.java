94c862bdabd0d94af198f7cf7327fcce643820f7
Mahwish-Inamdar <mehwishinamdar1234@gmail.com>
2023-11-19 21:43:58 +0530

    Java Algorithms

--- My.java
+++ My.java
@@ -1,76 +1,128 @@
-package com.learn.Project;
+class My {
+    // Method to check if the array is sorted
+    public static boolean checkSort(int[] arr) {
+        // Iterate through the array and check if each element is less than or equal to the next one
+        for (int i = 0; i <= arr.length - 2; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false; // If any pair of elements is out of order, return false
+            }
+        }
+        return true; // If all pairs are in order, return true
+    }
 
+    // Method to perform linear search
+    public static void linearSearch(int[] arr, int key) {
+        // Iterate through the array to find the key
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (key == arr[i]) {
+                System.out.println("Key found at: " + i + " using The linear Search");
+                System.exit(0); // Exit the program after finding the key
+            }
+        }
+        System.out.println("Key not found");
+    }
 
-public class My 
-{	
-	//CheckSorted Method
-	public static boolean checkSort(int [] arr)
-	{
-		for(int i=0; i<=arr.length-2; i++)
-		{
-			if(arr[i]>arr[i+1])
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	//Linear Search 
-	public static void linearSearch(int [] arr, int key)
-	{
-		for(int i=0; i<=arr.length-1; i++)
-		{
-			if(key == arr[i])
-			{
-				System.out.println("Key found at: "+i+" using The linear Search");
-				System.exit(0);
-			}
-		}
-		System.out.println("Key not found");
-	}
-	//Binary Search
-	public static void BinarySearch(int [] arr, int key1)
-	{
-		int low = 0;
-		int high = arr.length;
-		
-		while(low<=high)
-		{
-			int mid = (low+high)/2;
-			if(key1 == arr[mid])
-			{
-				System.out.println("Key found at : "+mid+ " using Binary Search");
-				return;
-			}
-			else if(key1 >= arr[mid])
-			{
-				low = mid+1;
-			}
-			else
-			{
-				high = mid-1;
-			}
-		}
-		System.out.println("Key not found");
-	}
-	//BubbleSorting Algorithm
-	public static int [] bubbleSort(int [] arr)
-	{
-		for(int i=0; i<= arr.length-2; i++)
-		{
-			for(int j=0; j<= arr.length-i-2; j++)
-			{
-				if(arr[j]>arr[j+1])
-				{
-					int temp = arr[j+1];
-					arr[j+1] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-		return arr;
-	}
+    // Method to perform binary search
+    public static void BinarySearch(int[] arr, int key1) {
+        int low = 0;
+        int high = arr.length;
+
+        // Binary search algorithm
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key1 == arr[mid]) {
+                System.out.println("Key found at : " + mid + " using Binary Search");
+                return;
+            } else if (key1 >= arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        System.out.println("Key not found");
+    }
+
+    // Method to perform bubble sort
+    public static int[] bubbleSort(int[] arr) {
+        int temp;
+        // Bubble sort algorithm
+        for (int i = 0; i <= arr.length - 2; i++) {
+            for (int j = 0; j <= arr.length - i - 2; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;  // Swap elements using temp
+                }
+            }
+        }
+        return arr;
+    }
+
+    // Method to perform insertion sort
+    public static int[] insertionSort(int[] arr) {
+        int n = arr.length;
+        // Insertion sort algorithm
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+
+    // Method to perform selection sort
+    public static int[] selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // Selection sort algorithm
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+    // Method to find the minimum element in the array
+    public static int findMinElement(int[] arr) {
+        if (arr.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int min = arr[0];
+        // Iterate through the array to find the minimum element
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    // Method to find the maximum element in the array
+    public static int findMaxElement(int[] arr) {
+        if (arr.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int max = arr[0];
+        // Iterate through the array to find the maximum element
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
-
-	
--- MyApp.java
+++ MyApp.java
@@ -4,81 +4,112 @@ import java.util.Scanner;
 
 public class MyApp {
 
-	public static void main(String[] args)
-	{
-		Scanner scan = new Scanner(System.in);
-		//Creating the array
-		System.out.println("Enter the length of the Array");
-		int [] arr = new int [scan.nextInt()];
-		
-		
-		//Storing the elements inside the array
-		System.out.println("Enter the Elements to be stored in the array");
-		for(int i=0; i<=arr.length-1; i++)
-		{
-			arr[i] = scan.nextInt();
-		}
-		
-		//Printing or asking all the options for user to press
-		System.out.println("Press 1 to check whether Array is sorted or not");
-		System.out.println("Press 2 to perform linear Search");
-		System.out.println("Press 3 to perform Binary Search");
-		System.out.println("Press 4 to sort an array using Bubble sort ");
-		System.out.println("Press 5 to sort an array using insertion sort ");
-		System.out.println("Press 6 to sort an array using selection sort");
-		System.out.println("Press 7 to print minimum element in Array ");
-		System.out.println("Press 8 to print maximum element in Array");
-		//storing in the variable
-		int num = scan.nextInt();
-		
-		switch(num)
-		{
-		case 1:
-			boolean status = My.checkSort(arr);
-			if(status == true)
-			{
-				System.out.println("The Given Array is Sorted");
-			}
-			else
-			{
-				System.out.println("The Given Array is not in Sorted order");
-			}
-			break;
-		case 2:
-			System.out.println("Enter the key to be searched in that array");
-			int key = scan.nextInt();
-			My.linearSearch(arr , key);
-			break;
-			
-		case 3:
-			System.out.println("Enter the key to be searched in that array");
-			int key1 = scan.nextInt();
-			My.BinarySearch(arr, key1);
-			break;
-			
-		case 4:
-			System.out.println("Array Elements before the sorted are:");
-			for(int x : arr)
-			{
-				System.out.print(x+ " ");
-			}
-			System.out.println();
-			int[] sortedArray = My.bubbleSort(arr);
-			System.out.print("Using Bubble Sort Algorithem The Array elements are sorted:");
-			System.out.println();
-			for(int  x : sortedArray)
-			{
-				System.out.print(x + " ");
-			}
-			break;
-			
-		case 5:
-			
-		default:
-            System.out.println("Invalid option. Please choose a valid option.");
+    public static void main(String[] args) {
+        // Create a Scanner object to take input from the user
+        Scanner scan = new Scanner(System.in);
 
-		}
+        // Creating the array by taking the length as user input
+        System.out.println("Enter the length of the Array");
+        int[] arr = new int[scan.nextInt()];
 
-	}
+        // Storing the elements inside the array
+        System.out.println("Enter the Elements to be stored in the array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
 
+        // Printing or asking all the options for the user to press
+        System.out.println("Press 1 to check whether Array is sorted or not");
+        System.out.println("Press 2 to perform linear Search");
+        System.out.println("Press 3 to perform Binary Search");
+        System.out.println("Press 4 to sort an array using Bubble sort ");
+        System.out.println("Press 5 to sort an array using insertion sort ");
+        System.out.println("Press 6 to sort an array using selection sort");
+        System.out.println("Press 7 to print minimum element in Array ");
+        System.out.println("Press 8 to print maximum element in Array");
+
+        // Storing the user's choice in the variable
+        int num = scan.nextInt();
+
+        switch (num) {
+            // Check whether the array is sorted
+            case 1:
+                boolean status = My.checkSort(arr);
+                if (status) {
+                    System.out.println("The Given Array is Sorted");
+                } else {
+                    System.out.println("The Given Array is not in Sorted order");
+                }
+                break;
+            // Perform linear search
+            case 2:
+                System.out.println("Enter the key to be searched in that array");
+                int key = scan.nextInt();
+                My.linearSearch(arr, key);
+                break;
+            // Perform binary search
+            case 3:
+                System.out.println("Enter the key to be searched in that array");
+                int key1 = scan.nextInt();
+                My.BinarySearch(arr, key1);
+                break;
+            // Sort array using Bubble Sort
+            case 4:
+                System.out.println("Array Elements before sorting:");
+                for (int x : arr) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+                int[] bubbleSortedArray = My.bubbleSort(arr.clone());
+                System.out.print("Using Bubble Sort Algorithm, the Array elements are sorted: ");
+                for (int x : bubbleSortedArray) {
+                    System.out.print(x + " ");
+                }
+                break;
+            // Sort array using Insertion Sort
+            case 5:
+                System.out.println("Array Elements before sorting:");
+                for (int x : arr) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+                int[] insertionSortedArray = My.insertionSort(arr.clone());
+                System.out.print("Using Insertion Sort Algorithm, the Array elements are sorted: ");
+                for (int x : insertionSortedArray) {
+                    System.out.print(x + " ");
+                }
+                break;
+            // Sort array using Selection Sort
+            case 6:
+                System.out.println("Array Elements before sorting:");
+                for (int x : arr) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+                int[] selectionSortedArray = My.selectionSort(arr.clone());
+                System.out.print("Using Selection Sort Algorithm, the Array elements are sorted: ");
+                for (int x : selectionSortedArray) {
+                    System.out.print(x + " ");
+                }
+                break;
+            // Print minimum element in the array
+            case 7:
+                int minElement = My.findMinElement(arr);
+                System.out.println("Minimum Element in the Array: " + minElement);
+                break;
+            // Print maximum element in the array
+            case 8:
+                int maxElement = My.findMaxElement(arr);
+                System.out.println("Maximum Element in the Array: " + maxElement);
+                break;
+            // Handle invalid option
+            default:
+                System.out.println("Invalid option. Please choose a valid option.");
+        }
+
+        // Close the Scanner to prevent resource leaks
+        scan.close();
+    }
 }
+
+
